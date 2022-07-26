416badec5279872ff013de33cf596a1d6e44752f
kolson463 <98200634+kolson463@users.noreply.github.com>
2022-07-25 23:18:45 -0500

    Added some color and improved navigation

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ async function handleDownload(req, res) {
   }
   file.downloadCount++;
   await file.save();
-  console.log(file.downloadCount);
+  //console.log(file.downloadCount);
 
   res.download(file.path, file.originalName);
 }
